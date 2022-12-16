83133886b18f03d9b19c8d2bc83a7965da667e97
Haze <70538228+HazeWorksOnFiveMServer@users.noreply.github.com>
2022-12-16 19:44:53 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,42 +1,42 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'your name'
-description 'your description'
+author 'HAZELIFE'
+description 'Package'
 
 files {
 -- Vehicles
-    'data/**/**/*.meta',
     'data/**/**/*.xml',
+    'data/**/**/*.ycd',
     'data/**/**/*.dat',
+    'data/**/**/*.meta',
     'data/**/**/*.ytyp',
-	'data/**/**/*.ycd',
--- Audio	
-	'audioconfig/*.dat151.rel',
-	'audioconfig/*.dat54.rel',
-	'audioconfig/*.dat10.rel',
-	'sfx/**/*.awc',
+-- Audio    
+    'audioconfig/*_amp.dat10.rel',
+    'audioconfig/*_game.dat151.rel',
+    'audioconfig/*_sounds.dat54.rel',
+    'sfx/**/*.awc',
 -- Peds
-	'stream/**/**/*.ydd',
-	'stream/**/**/*.yft',
-	'stream/**/**/*.ymt',
-	'stream/**/**/*.ytd'
+    'stream/**/**/*.ydd',
+    'stream/**/**/*.yft',
+    'stream/**/**/*.ymt',
+    'stream/**/**/*.ytd'
 }
 
-client_script 'names.lua'
+client_script '*.lua'
 
 -- Vehicles
-data_file 'HANDLING_FILE'                        'data/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'                 'data/**/vehiclelayouts.meta'
-data_file 'VEHICLE_METADATA_FILE'                'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE'                         'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE'               'data/**/carvariations.meta'
 data_file 'DLCTEXT_FILE'                         'data/**/dlctext.meta'
+data_file 'VEHICLE_METADATA_FILE'                'data/**/vehicles.meta'
+data_file 'HANDLING_FILE'                        'data/**/handling.meta'
+data_file 'VEHICLE_VARIATION_FILE'               'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'                 'data/**/vehiclelayouts.meta'
 data_file 'CARCONTENTUNLOCKS_FILE'               'data/**/carcontentunlocks.meta'
 -- Audio
-data_file 'AUDIO_SYNTHDATA'                      'audioconfig/*_amp.dat'
-data_file 'AUDIO_GAMEDATA'                       'audioconfig/*_game.dat'
-data_file 'AUDIO_SOUNDDATA'                      'audioconfig/*_sounds.dat'
+data_file 'AUDIO_SYNTHDATA'                      'audioconfig/*_amp.dat10.rel'
+data_file 'AUDIO_GAMEDATA'                       'audioconfig/*_game.dat151.rel'
+data_file 'AUDIO_SOUNDDATA'                      'audioconfig/*_sounds.dat54.rel'
 data_file 'AUDIO_WAVEPACK'                       'sfx/dlc_*'
 -- Peds
 data_file 'PED_METADATA_FILE'                    'data/**/peds.meta'
